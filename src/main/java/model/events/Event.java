39fb2753ed5d8606b512947858bd6128cdbf91eb
ramtinkh <ramtinkhoshnevis1@gmail.com>
2021-05-06 20:10:07 +0430

    ManuallyActivation added

--- src/main/java/model/events/Event.java
+++ src/main/java/model/events/Event.java
@@ -1,26 +1,24 @@
 package model.events;
 
 import controller.enums.GameEnums.SideOfFeature;
+import model.events.eventChildren.ManuallyActivation;
 import model.gameprop.BoardProp.MagicHouse;
 import model.gameprop.BoardProp.MonsterHouse;
 import model.gameprop.BoardProp.PlayerBoard;
 import model.gameprop.GameInProcess;
 
 public class Event {
-    public void activeEffects(){
+    public void activeEffects() {
         PlayerBoard currentPlayerBoard = GameInProcess.getGame().getPlayer(SideOfFeature.CURRENT).getBoard();
-        PlayerBoard opponentPlayerBoard = GameInProcess.getGame().getPlayer(SideOfFeature.OPPONENT).getBoard();
-        for (MonsterHouse monsterHouse : currentPlayerBoard.getMonsterHouse()) {
-            monsterHouse.getMonsterCard().activeEffectsByEvent(this);
+        if (this instanceof ManuallyActivation) {
+            GameInProcess.getGame().getCardProp().getCard().activeEffectsByEvent(this);
+        } else {
+            for (MonsterHouse monsterHouse : currentPlayerBoard.getMonsterHouse()) {
+                monsterHouse.getMonsterCard().activeEffectsByEvent(this);
+            }
+            for (MagicHouse magicHouse : currentPlayerBoard.getMagicHouse()) {
+                magicHouse.getMagicCard().activeEffectsByEvent(this);
+            }
         }
-        for (MagicHouse magicHouse : currentPlayerBoard.getMagicHouse()) {
-            magicHouse.getMagicCard().activeEffectsByEvent(this);
-        }
-//        for (MonsterHouse monsterHouse : opponentPlayerBoard.getMonsterHouse()) {
-//            monsterHouse.getMonsterCard().activeEffectsByEvent(this);
-//        }
-//        for (MagicHouse magicHouse : opponentPlayerBoard.getMagicHouse()) {
-//            magicHouse.getMagicCard().activeEffectsByEvent(this);
-//        }
     }
 }
---
+++ src/main/java/model/events/eventChildren/ManuallyActivation.java
@@ -0,0 +1,6 @@
+package model.events.eventChildren;
+
+import model.events.Event;
+
+public class ManuallyActivation extends Event {
+}
